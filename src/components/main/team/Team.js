c5e310a16f0dd2a90acc9c2d88937876bf722401
nicolas <nicolas.goulartsouza123@gmail.com>
2022-07-11 19:03:25 -0300

    correcao title team

--- src/components/main/team/Team.js
+++ src/components/main/team/Team.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import * as S from './Styles'
+import styled from 'styled-components'
 import SearchTeam from './search/SearchTeam'
 import TitleBox from '../styles-texts/TitleBox'
 
 export default function Team(){
 
+  const TitleTeam = styled(TitleBox)`
+    @media(max-width: 1000px){
+      text-align: center;
+    }
+  `
+
   return(
       <S.TeamSection id="team">
-        <TitleBox>Equipe</TitleBox>
+        <TitleTeam>Equipe</TitleTeam>
         <SearchTeam/>
       </S.TeamSection>
   )
